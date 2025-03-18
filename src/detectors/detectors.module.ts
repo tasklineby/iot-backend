af393bb59bd1cfcf60930f975bdd6380fd68a05e
emaldie <fakeyamuxa@gmail.com>
2025-03-19 02:58:15 +0300

    fix: import jwt module in detectors module

--- src/detectors/detectors.module.ts
+++ src/detectors/detectors.module.ts
@@ -3,10 +3,12 @@ import { DetectorsService } from './detectors.service';
 import { DetectorsController } from './detectors.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DetectorEntity } from './entities/detector.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DetectorEntity])],
+  imports: [TypeOrmModule.forFeature([DetectorEntity]), JwtModule],
   controllers: [DetectorsController],
   providers: [DetectorsService],
+  exports: [DetectorsService],
 })
 export class DetectorsModule {}
