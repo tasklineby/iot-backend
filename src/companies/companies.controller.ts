88e1e16045661bd5b409c28204f770cbde34db7c
emaldie <fakeyamuxa@gmail.com>
2025-03-19 09:38:35 +0300

    chore: fix companies end-point

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -25,9 +25,9 @@ export class CompaniesController {
     return this.companiesService.findAll();
   }
 
-  @Get(':id')
-  getId(@Param('id') id: string) {
-    return this.companiesService.findOne({ id: +id });
+  @Get(':code')
+  getId(@Param('code') code: string) {
+    return this.companiesService.findOne({ code: code });
   }
 
   @Patch(':id')
